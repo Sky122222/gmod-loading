9916eb0b6daa4ad2a52fbfc4d026e572970e9b57
Sky122222 <f21840346@gmail.com>
2025-12-25 00:44:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -97,9 +97,9 @@ function GameDetails(servername, serverurl, mapname, maxplayers, steamid, gamemo
 // ============================================
 
 const songs = [
-  { file: "The Blood of Kings.mp3", title: "The Blood of Kings - SWTOR" },
-  { file: "Coruscant, The Capital.mp3", title: "Coruscant, The Capital - SWTOR" },
-  { file: "Dromund Kaas,the Seat of Power.mp3", title: "Dromund Kaas,the Seat of Power - SWTOR" },
+  { file: "the-blood-of-kings.mp3", title: "The Blood of Kings" },
+  { file: "coruscant-the-capital.mp3", title: "Coruscant, The Capital" },
+  { file: "dromund-kaas-the-seat-of-power.mp3", title: "Dromund Kaas, The Seat of Power" },
 ]
 
 let currentSongIndex = 0
@@ -205,7 +205,7 @@ function updateVolumeIcon() {
     if (isMuted) {
       // Muted Icon
       volumeIcon.innerHTML =
-        '<path d="M16.5 12c0-1.77-1.02-3.29-2.5-4.03v2.21l2.45 2.45c.03-.2.05-.41.05-.63zm2.5 0c0 .94-.2 1.82-.54 2.64l1.51 1.51C20.63 14.91 21 13.5 21 12c0-4.28-2.99-7.86-7-8.77v2.06c2.89.86 5 3.54 5 6.71zM4.27 3L3 4.27 7.73 9H3v6h4l5 5v-6.73l4.25 4.25c-.67.52-1.42.93-2.25 1.18v2.06c1.38-.31 2.63-.95 3.69-1.81L19.73 21 21 19.73l-9-9L4.27 3zM12 4L9.91 6.09 12 8.18V4z"/>'
+        '<path d="M16.5 12c0-1.77-1.02-3.29-2.5-4.03v2.21l2.45 2.45c.03-.2.05-.41.05-.63zm2.5 0c0 .94-.2 1.82-.54 2.64l1.51 1.51C20.63 14.91 21 13.5 21 12c0-4.28-2.99-7.86-7-8.77v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z"/>'
     } else {
       // Volume Icon
       volumeIcon.innerHTML =
